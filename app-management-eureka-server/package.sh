12e5e3e02e6edb49cc739acb00fa9df65469b093
Administrator <admin@example.com>
2019-12-29 15:06:51 +0800

    fix:修改shell文件

--- app-management-eureka-server/package.sh
+++ app-management-eureka-server/package.sh
@@ -1,3 +1,8 @@
 #！/bin/bash
-mvn clean package -P dev
-cp target/*.jar /usr/local/cicd/eureka.jar
+mvn clean package -P pro
+cp target/*.jar /usr/local/spring-cloud/eureka.jar
+# shellcheck disable=SC2164
+cd /usr/local/spring-cloud/
+cp Dockerfile .
+docker build -t registry.cn-beijing.aliyuncs.com/application-spring-cloud/eureka:v1 .
+docker push registry.cn-beijing.aliyuncs.com/application-spring-cloud/eureka:v1
