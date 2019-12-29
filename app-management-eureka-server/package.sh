fc7db69f5e1a46a1cfd584a61ae5b3a00573177a
Administrator <admin@example.com>
2019-12-29 18:25:38 +0800

    fix:在K8S中部署高可用服务器

--- app-management-eureka-server/package.sh
+++ app-management-eureka-server/package.sh
@@ -4,6 +4,6 @@ cp target/*.jar /usr/local/spring-cloud/build/eureka.jar
 cp Dockerfile /usr/local/spring-cloud/build/Dockerfile
 # shellcheck disable=SC2164
 cd /usr/local/spring-cloud/build/
-docker rmi registry.cn-beijing.aliyuncs.com/application-spring-cloud/eureka:v1
-docker build -t registry.cn-beijing.aliyuncs.com/application-spring-cloud/eureka:v1 .
-docker push registry.cn-beijing.aliyuncs.com/application-spring-cloud/eureka:v1
+docker rmi registry.cn-beijing.aliyuncs.com/application-spring-cloud/eureka:v2
+docker build -t registry.cn-beijing.aliyuncs.com/application-spring-cloud/eureka:v2 .
+docker push registry.cn-beijing.aliyuncs.com/application-spring-cloud/eureka:v2
