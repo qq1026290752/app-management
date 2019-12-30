7afdaa93864251ffa6fb973afff6759859f1e183
Administrator <admin@example.com>
2019-12-30 21:07:58 +0800

    fix:增加k8s部署文件

--- app-management-eureka-server/package.sh
+++ app-management-eureka-server/package.sh
@@ -1,14 +1,14 @@
 #！/bin/bash
 mvn clean package -P prod
-var path = usr/local/spring-cloud/build/eureka/
-if [ ! -d  "$path" ]; then
-  mkdir  "$path"
+var path = 'usr/local/spring-cloud/build/eureka/'
+if [ ! -d  ${path} ]; then
+  mkdir  ${path}
 fi
-cp target/*.jar "${path}"eureka.jar
-cp Dockerfile "${path}"Dockerfile
-cp application-eureka.yaml "${path}"application-eureka.yaml
+cp target/*.jar ${path}eureka.jar
+cp Dockerfile ${path}Dockerfile
+cp application-eureka.yaml ${path}application-eureka.yaml
 # shellcheck disable=SC2164
-cd /usr/local/spring-cloud/build/
+cd ${path}
 docker rmi registry.cn-beijing.aliyuncs.com/application-spring-cloud/eureka:v2
 docker build -t registry.cn-beijing.aliyuncs.com/application-spring-cloud/eureka:v2 .
 docker push registry.cn-beijing.aliyuncs.com/application-spring-cloud/eureka:v2
