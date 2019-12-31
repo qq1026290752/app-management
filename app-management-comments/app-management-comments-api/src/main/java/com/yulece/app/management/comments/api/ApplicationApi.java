0b23537615ecb9938499f41ed55dc126728b01e6
Administrator <admin@example.com>
2020-01-01 00:45:26 +0800

    fix:修改项目循环依赖问题

---
+++ app-management-comments/app-management-comments-api/src/main/java/com/yulece/app/management/comments/api/ApplicationApi.java
@@ -0,0 +1,17 @@
+package com.yulece.app.management.comments.api;
+
+/**
+ * Copyright © 2019 eSunny Info. Tech Ltd. All rights reserved.
+ *
+ * @author wangyichao@28ph.cn
+ * @Title: ApplicationApi
+ * @Package com.yulece.app.management.comments.api
+ * @Description:
+ * @Date 2020-01-01 00:44
+ **/
+public class ApplicationApi {
+
+    public static void main(String[] args) {
+
+    }
+}
